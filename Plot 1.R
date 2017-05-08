395b1063a47652b6d348932f9923ae35d3e4c8a5
abhishek374 <abhishek.anand374@gmail.com>
2017-05-08 20:41:45 +0300

    Add files via upload

---
+++ Plot 1.R
@@ -0,0 +1,17 @@
+
+#Get the path of working directory
+path <- getwd()
+
+#Download the zip file there
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",destfile = paste(path, "pollutiondata.zip",sep = "/"))
+
+#read the data into R
+scc_pm25 <- readRDS("summarySCC_PM25.RDS")
+
+#Aggregate the Emissions data by Year
+yearbytotal <- aggregate(Emissions ~ Year, scc_pm25,FUN = "sum")
+
+#Plot the Total Emissions by Year
+png("plot1.png",width = 480, height = 480)
+barplot(yearbytotal$Emissions,names = yearbytotal$year, xlab = "Years",ylab = "Total Emissions",main = "Total Emissions by Year")
+dev.off()
